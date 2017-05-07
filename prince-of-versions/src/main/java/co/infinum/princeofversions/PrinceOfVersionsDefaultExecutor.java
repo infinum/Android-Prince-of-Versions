6dca5ed9ff10b2bdc1e5c503fa2244fc98551b9c
Antun Flaš <antun.flas@infinum.hr>
2017-05-07 16:11:22 +0200

    Add default executor

--- prince-of-versions/src/main/java/co/infinum/princeofversions/PrinceOfVersionsDefaultExecutor.java
+++ prince-of-versions/src/main/java/co/infinum/princeofversions/PrinceOfVersionsDefaultExecutor.java
@@ -1,12 +1,24 @@
 package co.infinum.princeofversions;
 
+import android.support.annotation.NonNull;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
 public class PrinceOfVersionsDefaultExecutor implements Executor {
+
+    private static final ExecutorService SERVICE = Executors.newSingleThreadExecutor(new ThreadFactory() {
+        @Override
+        public Thread newThread(@NonNull Runnable r) {
+            Thread t = new Thread(r, "PrinceOfVersions Thread");
+            t.setDaemon(true);
+            return t;
+        }
+    });
 
     @Override
     public void execute(Runnable runnable) {
-        Thread t = new Thread(runnable, "PrinceOfVersions Thread");
-        t.setPriority(Thread.NORM_PRIORITY);
-        t.setDaemon(true);
-        t.start();
+        SERVICE.submit(runnable);
     }
 }
