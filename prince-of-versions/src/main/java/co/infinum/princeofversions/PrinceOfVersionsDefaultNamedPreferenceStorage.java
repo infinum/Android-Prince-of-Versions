7aeb6992736ccf1752946b26dda9a73f37940b9b
Marko Docevski <marko.docevski@infinum.hr>
2018-08-09 15:54:46 +0200

    Moved PREF_FILE name to Storage file from the strings file.

--- prince-of-versions/src/main/java/co/infinum/princeofversions/PrinceOfVersionsDefaultNamedPreferenceStorage.java
+++ prince-of-versions/src/main/java/co/infinum/princeofversions/PrinceOfVersionsDefaultNamedPreferenceStorage.java
@@ -6,11 +6,12 @@ import android.content.SharedPreferences;
 class PrinceOfVersionsDefaultNamedPreferenceStorage implements Storage {
 
     private static final String KEY = "PrinceOfVersions_LastNotifiedUpdate";
+    private static final String PREF_FILE_NAME = "co.infinum.princeofversions.PREF_FILE";
 
     private SharedPreferences sp;
 
     PrinceOfVersionsDefaultNamedPreferenceStorage(Context context) {
-        sp = context.getSharedPreferences(context.getString(R.string.prince_of_versions_shared_preference_file), Context.MODE_PRIVATE);
+        sp = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
     }
 
     @Override
