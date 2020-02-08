2bc9c38d28e8dba7236e2a05550cb0bfc478b8ed
Antun Flaš <antun.flas@infinum.hr>
2020-02-08 09:24:14 +0100

    Rename consts

--- ExampleApp/src/main/java/co/infinum/povexampleapp/PrinceRequirementsChecker.java
+++ ExampleApp/src/main/java/co/infinum/povexampleapp/PrinceRequirementsChecker.java
@@ -16,8 +16,8 @@ public class PrinceRequirementsChecker implements RequirementChecker {
     /**
      * Minimum SDK for mandatory version
      */
-    private static final String MANDATORY_MIN_SDK = "requiredAndroidVersion";
-    private static final String REQUIRED_LETTER_LENGHT = "requiredNumberOfLetters";
+    private static final String REQUIRED_ANDROID_VERSION = "requiredAndroidVersion";
+    private static final String REQUIRED_LETTER_LENGTH = "requiredNumberOfLetters";
 
     private int version;
 
@@ -27,14 +27,14 @@ public class PrinceRequirementsChecker implements RequirementChecker {
 
     @Override
     public boolean checkRequirements(JSONObject data) throws JSONException {
-        if (data.has(MANDATORY_MIN_SDK)) {
-            int min = data.getInt(MANDATORY_MIN_SDK);
+        if (data.has(REQUIRED_ANDROID_VERSION)) {
+            int min = data.getInt(REQUIRED_ANDROID_VERSION);
             if (version < min - 5) {
                 return false;
             }
         }
-        if (data.has(REQUIRED_LETTER_LENGHT)) {
-            int min = data.getInt(REQUIRED_LETTER_LENGHT);
+        if (data.has(REQUIRED_LETTER_LENGTH)) {
+            int min = data.getInt(REQUIRED_LETTER_LENGTH);
             return min < 10;
         }
         return true;
